3a3c98559ab0d1134335e0ed407937e59338c5de
ChyengJason <654675991@qq.com>
2017-01-14 23:00:26 +0800

    modify

---
+++ app/src/main/java/com/jscheng/mr_horse/utils/Configs.java
@@ -0,0 +1,18 @@
+package com.jscheng.mr_horse.utils;
+
+/**
+ * Created by cheng on 17-1-14.
+ */
+public class Configs {
+    /**
+     *  友盟统计
+     */
+    public final static String YOU_MENG_APP_KEY = "123";
+
+    public final static String CHANNEL_ID="pre.im";
+
+    /**
+     * bugly统计
+     */
+    public final static String BUGLY_APP_ID = "123";
+}
